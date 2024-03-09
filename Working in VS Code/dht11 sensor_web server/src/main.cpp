454ef6e96becf048c4613915050ca399f0a5f985
MuhammadAli1132001 <engr.alikhan2001@gmail.com>
2024-03-09 15:51:20 +0500

    first commit

--- Working in VS Code/dht11 sensor_web server/src/main.cpp
+++ Working in VS Code/dht11 sensor_web server/src/main.cpp
@@ -4,83 +4,123 @@
 
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
-//#include <DHT_U.h>
+#include <ESPmDNS.h>
 
-#define SSID "Wipi"
-#define PSSWD "12345678"
+#define SSID "CST WIFI"
+#define PSSWD "iotdevs123"
 
 #define Dht_Sensor_Pin 9
 #define Dht_type DHT11
 float Humi = 0.0;
 float Temp = 0.0;
 
-IPAddress local_ip(192,168,1,1);
-IPAddress gateway(192,168,1,1);
-IPAddress subnet(255,255,255,0);
+
 
 DHT dht_sensor(Dht_Sensor_Pin, Dht_type);
 WebServer server(80);
 
 void handle_on_sensor();
 void handle_on_notfound();
+void handle_on_connect();
 
-void setup() {
-  // initialize serial communication at 115200 bits per second:
+const int led = 13;
+
+void handleRoot() {
+  digitalWrite(led, 1);
+  char temp[400];
+
+  Humi = dht_sensor.readHumidity();
+  Temp = dht_sensor.readTemperature();
+
+  snprintf(temp, 400,
+
+           "<html>\
+  <head>\
+    <meta http-equiv='refresh' content='5'/>\
+    <title>ESP32 Demo</title>\
+    <style>\
+      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
+    </style>\
+  </head>\
+  <body>\
+    <h1>Temperature and Humidity from ESP32!</h1>\
+    <p>Humidity: %02f</p>\
+    <p>Temperature: %02f</p>\
+  </body>\
+</html>",
+
+           Humi, Temp
+          );
+  server.send(200, "text/html", temp);
+  digitalWrite(led, 0);
+}
+
+void handleNotFound() {
+  digitalWrite(led, 1);
+  String message = "File Not Found bro\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+}
+
+void setup(void) {
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 0);
   Serial.begin(115200);
-  dht_sensor.begin();
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, PSSWD);
+  Serial.println("");
 
-  WiFi.softAP(SSID,PSSWD);
-  WiFi.softAPConfig(local_ip,gateway,subnet);
-  Serial.print("Starting to connnect\n");
-  delay(1000);
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
 
-  server.on("/Tempreture_Humidity", handle_on_sensor);
-  server.onNotFound(handle_on_notfound);
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(SSID);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 
+  if (MDNS.begin("esp32")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+  server.on("/inline", []() {
+  server.send(200, "text/plain", "this works as well");
+
+  });
+  
+  server.onNotFound(handleNotFound);
   server.begin();
-  Serial.print("server connected....\n");
-
+  Serial.println("HTTP server started");
 }
 
 void loop() {
 
   server.handleClient();
-  // read the analog / millivolts value for pin 2:
-   Humi = dht_sensor.readHumidity();
-   Temp = dht_sensor.readTemperature();
+                                                                  // read the analog / millivolts value for pin 2:
+  Humi = dht_sensor.readHumidity();
+  Temp = dht_sensor.readTemperature();
   
-  // print out the values you read:
+                                                                  // print out the values you read:
   Serial.print(Humi);
   Serial.println();
   Serial.print(Temp);
   Serial.println();
   
-  delay(2000);  // delay in between reads for as dht11 has 1khz frequency 
-}
-
-String web_html(){
-
-  String html_script = "<!DOCTYPE html> <html>";
-    html_script += "<head> <meta name \"viewport\" content =\"width = device-width, initial-scale=1.0, user-scalable=no\">\n";
-    html_script += "<title> sensors interfacing \n";
-    html_script += "</head> \n";
-    html_script += "<body> \n";
-
-    html_script += "<h2> Tempreture and Humidity </h2>\n";
-    html_script += "<h5> show reading </h5><a href=\"/Tempreture_Humidity\">click here</a>\n";
-    html_script += "</body>\n";
-    html_script += "</html>\n";
-
-  return html_script;
-}
-
-void handle_on_sensor(void)
-{
-  Serial.println("Sensors are sensed");
-  server.send(200, "text/html", web_html());
-}
-
-void handle_on_notfound()
-{
-  server.send(404, "text/plain","Not Found bro");
+  delay(1000);                                                     // delay in between reads for as dht11 has 1khz frequency 
 }
